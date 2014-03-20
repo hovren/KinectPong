fde86ea4a816c476d264c1d6aed9d79668eb7b7e
Hannes Ovrén <hannes.ovren@liu.se>
2014-03-20 12:58:17 +0100

    Game state design set

--- src/KinectPongGame.h
+++ src/KinectPongGame.h
@@ -9,7 +9,29 @@
 #define KINECTPONGGAME_H_
 
 #include <SDL2/SDL.h>
+#include <opencv2/core/core.hpp>
 #include "KinectInput.h"
+
+class KinectPongGame;
+
+class GameState {
+public:
+	virtual void handle_events(KinectInput* kinect_input) = 0;
+	virtual void handle_logic() = 0;
+	virtual void render() = 0;
+	virtual ~GameState(){};
+protected:
+	KinectPongGame* m_game;
+};
+
+enum GameStates {
+	STATE_NULL,
+	STATE_INTRO,
+	STATE_WAITFORPLAYERS,
+	STATE_PLAYING,
+	STATE_SHOWSCORE,
+	STATE_EXIT
+};
 
 class KinectPongGame {
 public:
@@ -18,16 +40,36 @@ public:
 	bool init(void);
 	void run(void);
 
-	void on_new_rgb_frame(cv::Mat& image);
-	void on_new_depth_frame(cv::Mat& image);
+	void set_next_state(int new_state);
+	SDL_Renderer* renderer() { return m_renderer; }
 
 private:
 	KinectInput m_kinect;
 
-	// Rendering primitives
+	// Game state handling (credit to lazyfoo.net)
+	GameState* m_current_state;
+	int m_state_id;
+	int m_next_state;
+	void change_state();
+
+	// Rendering
+	SDL_Texture* texture_from_mat(cv::Mat&);
 	SDL_Window* m_window;
 	SDL_Renderer* m_renderer;
 	SDL_Texture* m_rgb_tex;
+};
+
+
+class IntroState : public GameState {
+public:
+	IntroState(KinectPongGame*);
+	~IntroState();
+	void handle_events(KinectInput*);
+	void handle_logic(); // No logic required
+	void render();
+
+private:
+	SDL_Surface* m_background;
 };
 
 #endif /* KINECTPONGGAME_H_ */
