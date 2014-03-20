6354da8690d5175a6de611806e700c8fc1bf2f95
wallenberg <wallenberg@59-170.isylogon.isy.liu.se>
2014-03-20 12:11:57 +0100

    Bug workaround in eSpeak interface, but the first line is still garbled sometimes...

--- src/EspeakInterface.cpp
+++ src/EspeakInterface.cpp
@@ -15,11 +15,13 @@ EspeakInterface::EspeakInterface()
 	m_voice = "default";
 	m_position = 0;
 	m_end_position = 0;
-	m_flags = espeakCHARS_AUTO;
+	m_flags = espeakCHARS_8BIT;
 	m_identifier = NULL;
 	m_buf_length = 500;
 	m_options = 0;
-	m_user_data = NULL;
+	espeak_Initialize(m_audio_output, m_buf_length, m_path.c_str(), m_options );
+	espeak_SetVoiceByName(m_voice.c_str());
+	speak("voice synth active");
 }
 
 EspeakInterface::~EspeakInterface()
@@ -29,9 +31,6 @@ EspeakInterface::~EspeakInterface()
 
 void EspeakInterface::speak(std::string text)
 {
-    espeak_Initialize(m_audio_output, m_buf_length, m_path.c_str(), m_options );
-    espeak_SetVoiceByName(m_voice.c_str());
-    std::cout << "Speaking " << text << std::endl;
     espeak_Synth(text.c_str(), text.size(), m_position, m_position_type, m_end_position, m_flags, m_identifier, m_user_data );
     espeak_Synchronize();
 }
--- src/espeak_interface_test.cpp
+++ src/espeak_interface_test.cpp
@@ -7,12 +7,14 @@
 
 #include "EspeakInterface.h"
 #include <iostream>
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
 	std::cout << "Testing the espeak synth..." << std::endl;
 	EspeakInterface my_espeak_interface;
-	my_espeak_interface.speak("Hello World!");
+	my_espeak_interface.speak("First phrase");
+	my_espeak_interface.speak("Second phrase");
 	return 0;
 }
 
