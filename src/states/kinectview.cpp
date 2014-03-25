8c7306716f193ee72f9d07d7d8f78138c3ecaf8a
Wallenberg <wallenberg@59-170.isylogon.isy.liu.se>
2014-03-25 14:14:10 +0100

    Added player mask visualisation to kinectview and waitforplayer states

--- src/states/kinectview.cpp
+++ src/states/kinectview.cpp
@@ -27,10 +27,30 @@ void KinectViewState::handle_events(KinectInput* kinect) {
 		cv::flip(rgb, rgb, 1);
 		cv::flip(depth, depth, 1);
 
-		//std::cout << "New frame at time " << SDL_GetTicks() / 1000.0 << std::endl;
+		m_game->get_image_processor()->set_player_masks(depth);
+		m_game->get_image_processor()->find_player_faces(rgb, depth);
+
 		SDL_DestroyTexture(m_rgb_tex);
 		SDL_DestroyTexture(m_depth_tex);
-		m_rgb_tex = m_game->texture_from_mat(rgb);
+
+		std::vector<cv::Mat> rgb_channels;
+		cv::split(rgb, rgb_channels);
+		cv::Mat left_player_mask;
+		m_game->get_image_processor()->get_left_player_mask(left_player_mask);
+		cv::Mat right_player_mask;
+		m_game->get_image_processor()->get_right_player_mask(right_player_mask);
+		cv::Mat left_player_contact_mask;
+		m_game->get_image_processor()->get_left_player_contact_mask(left_player_contact_mask);
+		cv::Mat right_player_contact_mask;
+		m_game->get_image_processor()->get_right_player_contact_mask(right_player_contact_mask);
+		cv::max(rgb_channels.at(0), left_player_mask, rgb_channels.at(0));
+		cv::max(rgb_channels.at(0), right_player_mask, rgb_channels.at(0));
+		cv::max(rgb_channels.at(1), left_player_contact_mask, rgb_channels.at(1));
+		cv::max(rgb_channels.at(1), right_player_contact_mask, rgb_channels.at(1));
+		cv::Mat rgb_tex;
+		cv::merge(rgb_channels, rgb_tex);
+		m_rgb_tex = m_game->texture_from_mat(rgb_tex);
+
 		cv::Mat depth_show;
 		double min_val, max_val;
 		cv::minMaxLoc(depth, &min_val, &max_val);
